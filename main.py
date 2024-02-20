0534407f08fd48bd82ff2c132d9efc8fa5907e48
Partha Mitra <mitra.partha@gmail.com>
2024-02-21 02:20:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def backup(event, context):
 #    for idx, dbName in enumerate(dbName):
     print("Backup started for " + dbName + " database" )
 #   Command to execute 
-    command = "mysqldump -alv --no-tablespaces --skip-lock-tables --host %s --user %s -p%s %s | gzip -c | aws s3 cp - s3://%s/%s.gz" % (
-            dbHost, dbUser, dbPass, dbName, S3_BUCKET, dbName + "_" + timestamp)
+    command = "mysqldump -alv --no-tablespaces --skip-lock-tables --host %s --user %s -p%s %s | gzip > ~/%s.gz" && aws s3 cp ~/*.gz s3://%s/backup/" % (
+            dbHost, dbUser, dbPass, dbName, dbName + "_" + timestamp, S3_BUCKET)
     #Execute (command)
     subprocess.Popen(command, shell=True).wait()
 #        if idx < len(dbName) - 1:
