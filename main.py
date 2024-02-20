d7d1bfdee75ce1e989741d7568d01350512e6d0c
Partha Mitra <mitra.partha@gmail.com>
2024-02-21 01:36:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ def backup(event, context):
     dbUser = os.environ['DB_USER']
     dbPass = os.environ['DB_PASS']
 # Split the variable into a list of values
-    dbName = dNames.split(',')    
+#    dbName = dNames.split(',')    
     
 #    print(dbHost)
-    print("%s %s ".format(dbHost, dbName))
-    for idx, dbName in enumerate(dbName):
-        print("Backup started for " + dbName + " database" )
-        # Command to execute 
-        command = "mysqldump -alv --no-tablespaces --skip-lock-tables --host %s --user %s -p%s %s | gzip -c | aws s3 cp - s3://%s/%s.gz" % (
-            dbHost, dbUser, dbPass, dbName, S3_BUCKET, dbName + "_" + timestamp)
+    print("%s %s ".format(dbHost, dbNames))
+#    for idx, dbName in enumerate(dbName):
+        print("Backup started for " + dbNames + " database" )
+#   Command to execute 
+    command = "mysqldump -alv --no-tablespaces --skip-lock-tables --host %s --user %s -p%s %s | gzip -c | aws s3 cp - s3://%s/%s.gz" % (
+            dbHost, dbUser, dbPass, dbNames, S3_BUCKET, dbName + "_" + timestamp)
     #Execute (command)
-        subprocess.Popen(command, shell=True).wait()
-        if idx < len(dbName) - 1:
-           print("MySQL " + dbName + "backup finished")
-#    return "backup finished"
+    subprocess.Popen(command, shell=True).wait()
+#        if idx < len(dbName) - 1:
+    print("MySQL " + dbNames + "backup finished")
+    return "backup finished"
