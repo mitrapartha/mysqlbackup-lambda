d945fc8d278e532afda3b1d34fb16b2f0b023e07
Partha Mitra <mitra.partha@gmail.com>
2024-02-21 01:48:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ def backup(event, context):
     print("Function started")
 
     dbHost = os.environ['DB_HOST']
-    dNames = os.environ['DB_NAMES']
+    dbName = os.environ['DB_NAME']
     dbUser = os.environ['DB_USER']
     dbPass = os.environ['DB_PASS']
 # Split the variable into a list of values
 #    dbName = dNames.split(',')    
     
 #    print(dbHost)
-    print("%s %s ".format(dbHost, dbNames))
+    print("%s %s ".format(dbHost, dbName))
 #    for idx, dbName in enumerate(dbName):
-    print("Backup started for " + dbNames + " database" )
+    print("Backup started for " + dbName + " database" )
 #   Command to execute 
     command = "mysqldump -alv --no-tablespaces --skip-lock-tables --host %s --user %s -p%s %s | gzip -c | aws s3 cp - s3://%s/%s.gz" % (
-            dbHost, dbUser, dbPass, dbNames, S3_BUCKET, dbName + "_" + timestamp)
+            dbHost, dbUser, dbPass, dbName, S3_BUCKET, dbName + "_" + timestamp)
     #Execute (command)
     subprocess.Popen(command, shell=True).wait()
 #        if idx < len(dbName) - 1:
-    print("MySQL " + dbNames + "backup finished")
+    print("MySQL " + dbName + "backup finished")
     return "backup finished"
